d7c401ceb4f221b262908737b3001beab714f248
wivest <120594169+wivest@users.noreply.github.com>
2024-11-24 20:37:55 +0100

    feat: create constructor

--- Cache/Cell.cs
+++ Cache/Cell.cs
@@ -3,6 +3,15 @@ namespace CacheSimulator.Cache;
 class Cell
 {
     public int Tag { get; set; }
+    public int FirstIterationUsed { get; set; }
     public int LastIterationUsed { get; set; }
     public int CountUsed { get; set; }
+
+    public Cell(int tag, int firstIterationUsed, int lastIterationUsed, int countUsed)
+    {
+        Tag = tag;
+        FirstIterationUsed = firstIterationUsed;
+        LastIterationUsed = lastIterationUsed;
+        CountUsed = countUsed;
+    }
 }
